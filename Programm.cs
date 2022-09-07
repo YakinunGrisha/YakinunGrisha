b1fff83c8da5a9ff37ab4e53de2cee4cb2c29905
Gregory <113060763+YakinunGrisha@users.noreply.github.com>
2022-09-07 23:33:02 +0300

    Create Programm.cs

---
+++ Programm.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace ConsoleMission2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello world");
+        /* is lust fun 
+        }
+    }
+}   
